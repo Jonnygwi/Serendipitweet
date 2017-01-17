41ed631213f0ec99f91e9396d09cd5cbc4295c19
Jonnygwi <jonnygwi@hotmail.co.uk>
2017-01-17 16:39:07 +0000

    added .env

--- bot.js
+++ bot.js
@@ -1,23 +1,24 @@
-var Twitter = require('twitter'),
-    config = require('./config'),
-    _ = require('underscore'),
-    twitterBot = new Twitter(config.keys),
-    Twit = require('twit'),
-    T = new Twit(config.keys)
-    terminals = {},
-    startWords = [],
-    wordStats = {},
-    fIds = [],
-    totalFollowers = 0,
-    currentFollowerCounter = 0,
-    howManySentences = 10
-    urls = [],
-    hashTags = [],
-    parameters = {
-        exclude_replies: true,
-        include_rts: false,
-        count: 200
-    }
+var Twitter     = require('twitter'),
+    config      = require('./config'),
+    _           = require('underscore'),
+    twitterBot  = new Twitter(config.keys),
+    Twit        = require('twit'),
+    T           = new Twit(config.keys)
+
+var terminals               = {},
+    startWords              = [],
+    wordStats               = {},
+    fIds                    = [],
+    totalFollowers          = 0,
+    currentFollowerCounter  = 0,
+    howManySentences        = 10,
+    urls                    = [],
+    hashTags                = [],
+    parameters              = {
+                                exclude_replies: true,
+                                include_rts: false,
+                                count: 200
+                              }
 
 // Get serendipitweets follower ids
 
@@ -28,12 +29,14 @@ T.get('followers/ids', {
 
     totalFollowers = (fIds.length < 300) ? fIds.length : 300 // Twitter may complain if we're calling its API too many times
 
+    // Once all followers have been harvested we need to get each of their statuses
     getNextFollowerStatuses()
 })
 
 
-
+// Gets the status of the follower
 function getNextFollowerStatuses() {
+    // Looping through all followers
     if (currentFollowerCounter < totalFollowers) {
         console.log(currentFollowerCounter + '. Getting tweets from ' + fIds[currentFollowerCounter])
         parameters.user_id = fIds[currentFollowerCounter]
@@ -41,15 +44,12 @@ function getNextFollowerStatuses() {
         currentFollowerCounter++ // increment counter
     } else {
         console.log(currentFollowerCounter + '. We should be done looping ')
-
         for (var i = 0; i < howManySentences; i++) {
             var sentence = makeMarkovSentence(3 + Math.floor(3 * Math.random()))
-            sentence = _.unescape(sentence) 
-                    
+                sentence = _.unescape(sentence)
             if (getRandomNumer() == 2) {
                     sentence = insertExtras(sentence)
             }
-            
             console.log('- ' + sentence)
         }
     }
@@ -60,26 +60,26 @@ function getUserStatuses(parameters) {
     // console.log(parameters)
     twitterBot.get('statuses/user_timeline', parameters, function (error, tweets, response) {
         if (!error) {
-            
-            for (var i = 0; i < tweets.length; i++) 
+
+            for (var i = 0; i < tweets.length; i++)
             {
                 var tweet = tweets[i]
-                
+
                 // HASHTAGS
                 if (tweet.entities.hashtags)
                 {
                     tweet.entities.hashtags.forEach(function(hashtag, key)
                     {
                         hashTags = _(hashTags.concat(hashtag.text)).unique()
-                    })	
-                }                
+                    })
+                }
                 // URLS
                 if (tweet.entities.urls)
                 {
                     tweet.entities.urls.forEach(function(url, key)
                     {
                         urls = _(urls.concat(url.expanded_url)).unique()
-                    })	
+                    })
                 }
 
                 // Tweet sanitation
@@ -88,7 +88,7 @@ function getUserStatuses(parameters) {
                 text = removeHashtags(text)
                 text = removeMentions(text)
                 text = removePunctuation(text)
-                
+
                 var words = text.split(' ')
 
                 terminals[words[words.length - 1]] = true
@@ -144,13 +144,13 @@ function makeMarkovSentence(minLength) {
         if (sentence.length > minLength && terminals.hasOwnProperty(word)) break
     }
     if (sentence.length < minLength) return makeMarkovSentence(minLength)
-    
+
     return sentence.join(' ')
 }
 
 // Adding extras
 function insertExtras(sentence) {
-    
+
     var randomHashtag = getRandomElement(hashTags)
     var randomUrl = getRandomElement(urls)
 
@@ -160,4 +160,3 @@ function insertExtras(sentence) {
 function getRandomNumer() {
     return Math.floor(Math.random() * 4) + 1
 }
-
--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-module.exports = 
+module.exports =
 {
     keys:
     {
-        consumer_key: 'YOUR_TWITTER_APP_CONSUMER_KEY',
-        consumer_secret: 'YOUR_TWITTER_APP_CONSUMER_SECRET',
-        access_token_key: 'YOUR_TWITTER_APP_TOKEN_KEY',
-        access_token_secret: 'YOUR_TWITTER_APP_TOKEN_SECRET'
+        consumer_key: 'MTjvpgllMg4g18da2BHrbBRFk',
+        consumer_secret: 'Oi7JYBVEmQe6zWISVpp2erKU22n2Glil3hanedR7l684Wr8XR1',
+        access_token_key: '1129155464-HETqtgwb0kz1Eii5KdNVzXfzk5dTPblDmy5QqBG',
+        access_token_secret: 'XYQxu01OdSSoF9NN7RxANuEBp14Yam75SpHmts7HxTIRo'
     }
-}   
+}
